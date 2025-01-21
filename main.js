d7d53ae52cfd720ed42c2edd6cc66588870f6e98
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-01-21 11:48:14 -0500

    Completed

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-// write your code here to make the tests pass
+const Library = function() {
+  const books = [];
+
+  const addBook = function (book) {
+    books.push(book);
+    return `${book.title} added to library`
+  }
+
+  const checkOutBook = function (book) {
+    if (books.includes(book)) {
+      book.setAttribute('checkedOut', true)
+    }
+  }
+
+  const returnBook = function (book) {
+    if (books.includes(book)) {
+      book.setAttribute('checkedOut', false)
+    }
+  }
+
+  return {addBook, checkOutBook, returnBook};
+}
+
+const Book = function (title, author) {
+  const attributes = {
+    checkedOut: false,
+    title,
+    author,
+  }
+
+  const getAttribute = function (name) {
+    if (attributes.hasOwnProperty(name)) {
+      return attributes[name];
+    }
+  }
+
+  const setAttribute = function (name, value) {
+    if (attributes.hasOwnProperty(name)) {
+      attributes[name] = value;
+    }
+  }
+
+  return {getAttribute, setAttribute};
+}
